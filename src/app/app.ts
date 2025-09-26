c8328088b53bb64e252ce8e7bb52979736be2bec
81andie <133395577+81andie@users.noreply.github.com>
2025-09-26 22:34:59 +0200

    Creación archivo+ instalación tailwind + configuración + componentes + navbar+hero

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./components/navbar/navbar";
+import { Inicio } from "./components/inicio/inicio";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Inicio],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
