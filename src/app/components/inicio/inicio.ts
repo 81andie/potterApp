2d21965a685583d9a8d4378d517a994792c2a256
81andie <133395577+81andie@users.noreply.github.com>
2025-09-27 11:58:05 +0200

    guardando seccion peliculas

--- src/app/components/inicio/inicio.ts
+++ src/app/components/inicio/inicio.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Hero } from "../hero/hero";
+import { Peliculas } from "../peliculas/peliculas";
+import { Libros } from "../libros/libros";
 
 @Component({
   selector: 'app-inicio',
-  imports: [Hero],
+  imports: [Hero, Peliculas, Libros],
   templateUrl: './inicio.html',
   styleUrl: './inicio.css'
 })
